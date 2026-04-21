f56ca373d11e045a68e7727d7b1500bc025aa640
junxiang <jimmy871117@gmail.com>
2026-04-21 17:41:48 +0800

    A-Eye v1 - ADAS-inspired driving alert PWA

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'aeye-v2';
+const FILES = ['/', '/index.html', '/app.js', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(
+    ks.filter(k => k !== CACHE).map(k => caches.delete(k))
+  )));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
