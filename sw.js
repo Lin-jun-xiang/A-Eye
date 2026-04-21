0a7f9116afb52f7b9bf1502a26b3f3066412004d
junxiang <jimmy871117@gmail.com>
2026-04-21 17:46:11 +0800

    fix: use relative paths for GitHub Pages

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'aeye-v2';
-const FILES = ['/', '/index.html', '/app.js', '/manifest.json'];
+const FILES = ['./', './index.html', './app.js', './manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
